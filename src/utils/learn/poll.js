2ebac2a1dac7808f939d0bd16aa053a94b9bfc31
chongtianbai <chongtianbai@myweimai.com>
2022-01-24 18:43:45 +0800

    添加一个轮询方法

---
+++ src/utils/learn/poll.js
@@ -0,0 +1,108 @@
+/*
+/!**
+ * 轮询调用requestStatus接口，直到状态非checking，查询完成
+ * 要求：轮询间隔为1s，5s没有结果抛出超时 timeout。
+ *!/
+// @ts-ignore
+const poll: () => Promise<"completed" | "error" | "timeout"> = async () => {
+    // TODO
+};
+
+type Status = "checking" | "completed" | "error";
+
+/!**
+ * 服务端接口 最快0.2s 最慢1.6s 返回结果 无需关心内部实现
+ * @return "checking" | "completed" | "error"
+ *!/
+function requestStatus(): Promise<Status> {
+    const random = Math.floor(Math.random() * 8);
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const res = weightRandom({ checking: 6, completed: 2, error: 1 });
+            resolve(res);
+        }, random * 200);
+    });
+}
+
+/!** --------------  其他工具函数 无需关心 -------------- *!/
+
+/!**
+ * 加权随机数
+ * @param weightObj
+ * @returns
+ *!/
+function weightRandom<T extends string>(weightObj: Record<T, number>): T {
+    // 获取权重值
+    let weightNumbers: number[] = Object.values(weightObj);
+    // 权重值总和
+    const sum = weightNumbers.reduce((total, current) => total + current);
+    // 获取一个随机数
+    let random = getRandomInt(1, sum + 1);
+    const keys = Object.keys(weightObj);
+    for (let index = 0; index < keys.length; index++) {
+        const key = keys[index] as T;
+        const value = weightObj[key];
+        if (random <= value) {
+            return key;
+        }
+        random = random - value;
+    }
+    return "" as T;
+}
+/!**
+ *
+ * @param min 最小值
+ * @param max 最大值
+ * @returns 随机数
+ *!/
+function getRandomInt(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //不含最大值，含最小值
+}
+
+/!**
+ * 测试是否通过
+ *!/
+Promise.all(
+    new Array(100).fill(1).map(async () => {
+        const time = performance.now();
+        const res = await poll();
+        return {
+            result: res,
+            time: (performance.now() - time) / 1000
+        };
+    })
+).then((pollRestus) => {
+    pollRestus.forEach((item) => {
+        if (!["completed", "error", "timeout"].includes(item.result)) {
+            throw new Error(`tests failed! shoud get "completed" | "error" | "timeout", but got ${JSON.stringify(item)}`);
+        }
+    });
+    console.info("Congratulations! tests passed", pollRestus);
+});
+
+export default poll;
+
+ const poll: () => Promise<"completed" | "error" | "timeout"> = async () => {
+  const maxTime = 5000;
+  const time = performance.now();
+  const getStatus: () => Promise<"completed" | "error" | "timeout"> = async () => {
+    // 超时
+    if (performance.now() - time > maxTime) {
+      return "timeout";
+    }
+    const res = await requestStatus();
+    // 非checking状态停止轮训
+    if (res !== "checking") {
+      return res;
+    }
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(getStatus());
+      }, 1000);
+    });
+  };
+  return getStatus();
+};
+*/
